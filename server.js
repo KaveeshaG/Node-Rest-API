3f301621a8f1d09d92821af3951e70d27f92cc36
KaveeshaG <uakaveeshagimhana@gmail.com>
2022-01-22 23:53:51 +0530

    commented PORT

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var HTTP_PORT = 8000
+// var HTTP_PORT = 8000
 
 // Start server
-app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
-});
+// app.listen(HTTP_PORT, () => {
+//     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+// });
 
 app.get("/api/users", (req, res, next) => {
     var sql = "select * from user"
